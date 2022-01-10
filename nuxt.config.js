fd564372bc87115ff15dd40cac91bce17f05fca3
Julien_P <jparis.py@gmail.com>
2022-01-10 12:54:15 +0100

    minor changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -123,7 +123,7 @@ export default {
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: '/',
-    proxyHeaders: false,
+    // proxyHeaders: false,
     credentials: false
   },
 
