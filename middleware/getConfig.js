88355e214e48f10169239fdbdd79fed5c8ce27e6
Julien_P <jparis.py@gmail.com>
2022-01-28 12:12:54 +0100

    delete comments

--- middleware/getConfig.js
+++ middleware/getConfig.js
@@ -11,7 +11,6 @@ export default async function ({
   // console.log( '-MW- getConfig > ... ' ) 
 
   if (!store.state.configFile) {
-    // store.dispatch('updateContentsRepo', env.CONFIG_APP.contentsRepo)
     store.dispatch('updateConfigFile', env.CONFIG_APP.configFile)
   }
 
