cadf5d489eb762bf5148d9d125b27cfc794e170e
Julien Paris <julien.paris@multi.coop>
2023-03-03 17:47:40 +0100

    trying to debug css ext loading

--- utils/utilsStyles.js
+++ utils/utilsStyles.js
@@ -2,6 +2,7 @@ export const createStyleLink = (url, isFont = false) => {
   const link = document.createElement('link')
   link.rel = 'stylesheet'
   link.type = 'text/css'
+  link.as = 'style'
   if (isFont) {
     link.type = 'font'
     link.as = 'font'
